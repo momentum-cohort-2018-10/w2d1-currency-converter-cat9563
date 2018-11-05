479538315545e3b2d0739ca705e4e9c68af539c5
Crystal Ramirez <crystal9563@gmail.com>
2018-11-05 17:21:24 -0500

    test multiple rates

--- test_currency.py
+++ test_currency.py
@@ -18,3 +18,11 @@ def test_convert_reveres():
         convert(
             rates=[("USD", "EUR", 0.74)], value=1, current='EUR', to='USD'),
         2) == 1.35
+        
+def test_convert_multiple_rates():
+    rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
+    assert round(convert(rates, value=10, current='USD', to='EUR'), 2) == 7.4
+    assert round(convert(rates, value=10, current='EUR', to='USD'), 2) == 13.51
+    assert round(convert(rates, value=10, current='EUR', to='JPY'),
+                 2) == 1459.49
+    assert round(convert(rates, value=100, current='JPY', to='EUR'), 2) == 0.69

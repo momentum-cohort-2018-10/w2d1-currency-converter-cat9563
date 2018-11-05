6207cfbda8be11dc36103f1f066a6d2bb3a88b0f
Crystal Ramirez <crystal9563@gmail.com>
2018-11-05 17:25:39 -0500

    raises assertion

--- test_currency.py
+++ test_currency.py
@@ -18,7 +18,7 @@ def test_convert_reveres():
         convert(
             rates=[("USD", "EUR", 0.74)], value=1, current='EUR', to='USD'),
         2) == 1.35
-        
+
 def test_convert_multiple_rates():
     rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
     assert round(convert(rates, value=10, current='USD', to='EUR'), 2) == 7.4
@@ -26,3 +26,8 @@ def test_convert_multiple_rates():
     assert round(convert(rates, value=10, current='EUR', to='JPY'),
                  2) == 1459.49
     assert round(convert(rates, value=100, current='JPY', to='EUR'), 2) == 0.69
+
+def test_converting_with_no_matching_rates():
+    rates = [("EUR", "JPY", 145.949)]
+    with pytest.raises(ValueError):
+        convert(rates, value=10, current='EUR', to='MXN')
